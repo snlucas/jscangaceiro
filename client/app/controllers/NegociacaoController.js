b630e5c8f7d7cac9ce806388b23046ed7fe36899
snlucas <lucas.lambda.101@gmail.com>
2024-12-20 11:37:29 -0300

    Save progress

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,16 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+  }
+
   adiciona(event) {
     event.preventDefault();
-    alert("Chamei ação no controller!");
+
+    console.log(this._inputData.value.split("-").reverse().join("/"));
+    console.log(this._inputQuantidade.value);
+    console.log(this._inputValor.value);
   }
 }
