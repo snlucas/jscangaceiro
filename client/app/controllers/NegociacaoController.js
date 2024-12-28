41a42a101003e1f5cdd31195768585de88c6d454
snlucas <lucas.lambda.101@gmail.com>
2024-12-28 00:35:23 -0300

    Save progress

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,9 +9,8 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(...this._inputData.value.split("-"));
     let negociacao = new Negociacao(
-      this._inputData.value,
+      DateConverter.paraData(this._inputData.value),
       parseInt(this._inputQuantidade.value),
       parseInt(this._inputValor.value)
     );
